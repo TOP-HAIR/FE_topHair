a45baf14eeabce3da1df4170a2fe5650e1d48b47
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2023-09-11 22:30:55 -0300

    adding base configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <div className="p-6">
+        <>app works</>
+        <Button>Primary</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
