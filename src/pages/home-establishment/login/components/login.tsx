3acab400f060242236e41573d4a2ebeebb6f48bd
william.matos@sptech.school <william.matos@sptech.school>
2023-09-19 00:49:07 -0300

    William-LoginPageFinalizada

--- src/pages/home-establishment/login/components/login.tsx
+++ src/pages/home-establishment/login/components/login.tsx
@@ -8,13 +8,28 @@ export const Loginpage = () => {
             <div className="h-screen w-screen flex justify-center items-center">
                 <div className="login-modal bg-primary-dark-cyan rounded-lg flex justify-center items-center flex-col">
                     <div className="w-full">
-                    <p className="text-white ml-3.5 text-sm">Voltar para o site</p>
+                    <p className="text-white ml-3.5 text-sm mt-2">Voltar para o site</p>
                     </div>
-                    <div className="h-4/5 w-full">
-                        <div className="w-full flex justify-center items-center">
-                        <img className="h-28 w-28" src={TophairIcon} alt="" />
+                    <div className="h-full w-full">
+                        <div className="w-full flex mt-4 justify-center">
+                        <img className="h-28 w-28" src={TophairIcon} alt="tophair-icon" />
                         </div>
-                        <div className="w-full h-full"></div>
+                        <div className="w-full h-80 text-white flex justify-center items-center flex-col">
+                            <div className="w-4/5 flex-col">
+                            <label htmlFor="Email:">Email:</label>
+                            <input className="input-login w-full text-black h-8 mb-6 rounded-sm"
+                             type="text" placeholder="Digite aqui seu Email" />
+                             </div>
+                             <div className="w-4/5 flex-col">
+                            <label htmlFor="Email:">Senha:</label>
+                            <input className="input-login w-full text-black h-8 mb-6 rounded-sm"
+                             type="password" placeholder="Digite aqui sua Senha" />
+                             </div>
+                             <div className="w-4/5 text-center">
+                                <p className="text-white ml-3.5 text-sm mt-2">Ainda não tem uma conta ? faça seu cadastro</p>
+                                <button className="w-full bg-primary-cyan h-9 mt-6 rounded-sm" type="button">ENTRAR</button>
+                             </div>
+                        </div>
                     </div>
                 </div>
             </div>
