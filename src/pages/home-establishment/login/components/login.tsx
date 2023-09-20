f743feffc06d256294404e62f7ece07c9d977b9e
william.matos@sptech.school <william.matos@sptech.school>
2023-09-20 17:49:12 -0300

    william-alteraçoes-loginpage

--- src/pages/home-establishment/login/components/login.tsx
+++ src/pages/home-establishment/login/components/login.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import TophairIcon from "../../../../assets/configs/img/logo/white-logo.png";
+import  EmailIcon  from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
 
 
 export const Loginpage = () => {
@@ -7,8 +9,8 @@ export const Loginpage = () => {
         <>
             <div className="h-screen w-screen flex justify-center items-center">
                 <div className="login-modal bg-primary-dark-cyan rounded-lg flex justify-center items-center flex-col">
-                    <div className="w-full">
-                    <p className="text-white ml-3.5 text-sm mt-2">Voltar para o site</p>
+                    <div className="w-full mt-2">
+                    <a href="/" className="text-white ml-3.5 text-sm text-cyan-400"><u>Voltar para o site</u></a>
                     </div>
                     <div className="h-full w-full">
                         <div className="w-full flex mt-4 justify-center">
@@ -16,17 +18,17 @@ export const Loginpage = () => {
                         </div>
                         <div className="w-full h-80 text-white flex justify-center items-center flex-col">
                             <div className="w-4/5 flex-col">
-                            <label htmlFor="Email:">Email:</label>
+                            <label htmlFor="Email:"><EmailIcon fontSize="small" className="mb-1 mr-1"/>Email:</label>
                             <input className="input-login w-full text-black h-8 mb-6 rounded-sm"
                              type="text" placeholder="Digite aqui seu Email" />
                              </div>
                              <div className="w-4/5 flex-col">
-                            <label htmlFor="Email:">Senha:</label>
+                            <label htmlFor="Email:"><LockIcon fontSize="small" className="mb-1 mr-1"/>Senha:</label>
                             <input className="input-login w-full text-black h-8 mb-6 rounded-sm"
                              type="password" placeholder="Digite aqui sua Senha" />
                              </div>
-                             <div className="w-4/5 text-center">
-                                <p className="text-white ml-3.5 text-sm mt-2">Ainda não tem uma conta ? faça seu cadastro</p>
+                             <div className="w-4/5">
+                                <p className="text-white ml-3.5 text-sm mt-2">Ainda não tem uma conta ? <a className="text-cyan-400" href="/"><u>faça seu cadastro aqui</u></a></p>
                                 <button className="w-full bg-primary-cyan h-9 mt-6 rounded-sm" type="button">ENTRAR</button>
                              </div>
                         </div>
