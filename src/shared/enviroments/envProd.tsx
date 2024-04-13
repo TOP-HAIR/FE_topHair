6ef5b1ff2bec41d4f535069346a51bf2595e93bd
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-04-13 18:17:54 -0300

    ajuste req

--- src/shared/enviroments/envProd.tsx
+++ src/shared/enviroments/envProd.tsx
@@ -1,6 +1,6 @@
 export const enviromentsProd = {
   production: true,
-  url: "http://34.205.172.82/api/",
+  url: "http://10.0.0.182:8080/api/",
   api: {
     TOKEN_API: "",
     URLBASE_API_USER: "http://localhost:8080/usuarios",
