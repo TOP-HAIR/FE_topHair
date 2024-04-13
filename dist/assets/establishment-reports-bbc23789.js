fe15da745a6f89002c2c75ddd2f298e805723ee9
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-04-13 18:18:32 -0300

    ajuste req

--- dist/assets/establishment-reports-bbc23789.js
+++
@@ -1,1 +0,0 @@
-import{j as e,O as s}from"./index-8da62feb.js";function r(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{r as default};
