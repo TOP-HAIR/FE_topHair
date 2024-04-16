a2d5cf3207b96c6a619ce6006545a1b7532bda04
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-04-15 22:13:53 -0300

    env prod

---
+++ dist/assets/report-dashboard-cfb97665.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-de52eb67.js";function s(){return r.jsx(r.Fragment,{children:r.jsx("p",{children:"Dashboard works"})})}export{s as default};
--- src/shared/enviroments/envProd.tsx
+++ src/shared/enviroments/envProd.tsx
@@ -1,6 +1,6 @@
 export const enviromentsProd = {
   production: true,
-  url: "http://52.206.175.42:8080/api/",
+  url: "http://54.89.27.121:8080/api/",
   api: {
     TOKEN_API: "",
     URLBASE_API_USER: "http://localhost:8080/usuarios",
