d4e7d3736f3fa990e337df31a68edd7556af38fd
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-04-22 00:50:33 -0300

    env

---
+++ dist/assets/establishment-employees-2bbe4e22.js
@@ -0,0 +1,1 @@
+import{j as e,O as s}from"./index-831be1b6.js";function l(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{l as default};
---
+++ dist/assets/report-dashboard-4b43b836.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-831be1b6.js";function s(){return r.jsx(r.Fragment,{children:r.jsx("p",{children:"Dashboard works"})})}export{s as default};
