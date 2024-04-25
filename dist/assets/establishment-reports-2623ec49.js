139527426904db9e0bc6ae40a68260631ee72f3c
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-04-24 21:06:08 -0300

    retiro pasta dist

--- dist/assets/establishment-reports-2623ec49.js
+++
@@ -1,1 +0,0 @@
-import{j as e,O as s}from"./index-9a70373a.js";function r(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{r as default};
