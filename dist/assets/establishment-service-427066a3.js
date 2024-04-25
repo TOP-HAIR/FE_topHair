df72077fb1be57a9cdfe1fbf8c6448de79ae63a3
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-04-24 21:40:07 -0300

    correção caminho

---
+++ dist/assets/establishment-service-427066a3.js
@@ -0,0 +1,1 @@
+import{j as e,O as s}from"./index-ea70fa53.js";function r(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{r as default};
