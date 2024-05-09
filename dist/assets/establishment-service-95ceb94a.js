5ec711a496807a6822c5ba801a52dfc42de9b11f
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-05-09 20:17:18 -0300

    env

--- dist/assets/establishment-service-95ceb94a.js
+++
@@ -1,1 +0,0 @@
-import{j as e,O as s}from"./index-e196f9a4.js";function r(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{r as default};
