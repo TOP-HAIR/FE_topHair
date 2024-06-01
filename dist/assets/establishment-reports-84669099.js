c25db09360039d94d732e96ac3230d85bc2ae109
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-06-01 17:08:31 -0300

    build: build com a env pi

--- dist/assets/establishment-reports-84669099.js
+++
@@ -1,1 +0,0 @@
-import{j as e,O as s}from"./index-b4deece9.js";function r(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{r as default};
---
+++ dist/assets/establishment-service-e149ca78.js
@@ -0,0 +1,1 @@
+import{j as e,O as s}from"./index-8c46529e.js";function r(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{r as default};
