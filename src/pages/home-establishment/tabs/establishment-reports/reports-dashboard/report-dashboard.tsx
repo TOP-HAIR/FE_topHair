5e3fb8f358e281d38c41fbcbfc977331ca986e16
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-06-16 15:41:39 -0300

    fix: Implementação com a nova Dashboard

--- src/pages/home-establishment/tabs/establishment-reports/reports-dashboard/report-dashboard.tsx
+++ src/pages/home-establishment/tabs/establishment-reports/reports-dashboard/report-dashboard.tsx
@@ -10,15 +10,11 @@ import { useLocation } from "react-router-dom";
 import NoContentComponent from "../../../../../components/noContent";
 import { BarChart } from "@mui/x-charts";
 
-export default function ReportDashboard() {
+export default function ReportDashboard({ dataInicio, dataFim }) {
   const [loadResponse, setloadResponse] = useState(false);
   const [resLenghtValid, setResLenghtValid] = useState(false);
   const [dadosRelatorio, setDadosRelatorio] = useState<MetricaEmpresa>();
   const [dadosChart, setDadosChart] = useState<DadoChart[]>([]);
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const dataInicio = queryParams.get("dataInicio");
-  const dataFim = queryParams.get("dataFim");
   let dataset;
   let valoresTotais;
   let valorMinimo;
@@ -26,75 +22,74 @@ export default function ReportDashboard() {
 
   const transformarDadosRelatorio = (
     dados: MetricaEmpresa
-  ): { semana: string; total: number }[] => {
+  ): { semana: string; total: number; rendimento: number }[] => {
     const semanaArray = [
-      { semana: "Domingo", total: dados?.qtdAgendaDomingo },
-      { semana: "Segunda", total: dados?.qtdAgendaSegunda },
-      { semana: "Terça", total: dados?.qtdAgendaTerca },
-      { semana: "Quarta", total: dados?.qtdAgendaQuarta },
-      { semana: "Quinta", total: dados?.qtdAgendaQuinta },
-      { semana: "Sexta", total: dados?.qtdAgendaSexta },
-      { semana: "Sábado", total: dados?.qtdAgendaSabado },
+      {
+        semana: "Domingo",
+        total: dados?.qtdAgendaDomingo,
+        rendimento: dados?.rendimentoDomingo,
+      },
+      {
+        semana: "Segunda",
+        total: dados?.qtdAgendaSegunda,
+        rendimento: dados?.rendimentoSegunda,
+      },
+      {
+        semana: "Terça",
+        total: dados?.qtdAgendaTerca,
+        rendimento: dados?.rendimentoTerca,
+      },
+      {
+        semana: "Quarta",
+        total: dados?.qtdAgendaQuarta,
+        rendimento: dados?.rendimentoQuarta,
+      },
+      {
+        semana: "Quinta",
+        total: dados?.qtdAgendaQuinta,
+        rendimento: dados?.rendimentoQuinta,
+      },
+      {
+        semana: "Sexta",
+        total: dados?.qtdAgendaSexta,
+        rendimento: dados?.rendimentoSexta,
+      },
+      {
+        semana: "Sábado",
+        total: dados?.qtdAgendaSabado,
+        rendimento: dados?.rendimentoSabado,
+      },
     ];
     console.log(semanaArray);
 
     return semanaArray;
   };
-  const xAxis = {
-    label: "Distance between home and office (km)",
-    scaleType: "band" as const,
-    data: [
-      "Domingo",
-      "Segunda",
-      "Terça",
-      "Quarta",
-      "Quinta",
-      "Sexta",
-      "Sábado",
-    ],
-  };
-  // useEffect(() => {
-  //   async function listarEmpresaRelatorio() {
-  //     const dadosrChart = transformarDadosRelatorio(dadosRelatorio);
-  //     setDadosChart(dadosrChart);
-  //     dataset = dadosrChart.map((item) => ({
-  //       semana: item.semana,
-  //       total: item.total,
-  //     }));
-
-  //     valoresTotais = dadosChart.map((item) => item.total);
-  //     valorMinimo = Math.min(...valoresTotais);
-  //     valorMaximo = Math.max(...valoresTotais);
-  //   }
-  //   listarEmpresaRelatorio();
-  // }, []);
 
   useEffect(() => {
     async function listarEmpresaRelatorio() {
-      // setloadResponse(false);
-      // try {
+      setloadResponse(false);
       const res = await getListarEmpresaRelatorioContext(dataInicio, dataFim);
-      const dadosChart = transformarDadosRelatorio(res as MetricaEmpresa);
-      setDadosChart(dadosChart);
-      dataset = dadosChart.map((item) => ({
-        semana: item.semana,
-        total: item.total,
-      }));
-      setDadosRelatorio(res as MetricaEmpresa);
 
-      valoresTotais = dadosChart.map((item) => item.total);
-      console.log(dadosChart);
-      valorMinimo = Math.min(...valoresTotais);
-      valorMaximo = Math.max(...valoresTotais);
-      setDadosChart(dadosChart);
-      console.log(res);
-      if (res.data == undefined || res.data.length == 0) {
-        // setResLenghtValid(true);
+      if (res == undefined || res.data.length == 0) {
+        setResLenghtValid(true);
+      } else {
+        const dadosChart = transformarDadosRelatorio(res as MetricaEmpresa);
+
+        setDadosChart(dadosChart);
+        dataset = dadosChart.map((item) => ({
+          semana: item.semana,
+          total: item.total,
+          rendimento: item.rendimento,
+        }));
+        setDadosRelatorio(res as MetricaEmpresa);
+
+        valoresTotais = dadosChart.map((item) => item.total);
+
+        valorMinimo = Math.min(...valoresTotais);
+        valorMaximo = Math.max(...valoresTotais);
+        setDadosChart(dadosChart);
       }
-      // setloadResponse(true);
-      // } catch (error) {
-      //   console.error("Erro ao buscar relatórios:", error);
-      // }
+      setloadResponse(true);
     }
 
     listarEmpresaRelatorio();
@@ -103,91 +98,137 @@ export default function ReportDashboard() {
   return (
     <>
       <section className="w-full">
-        {/* {!loadResponse ? (
+        {!loadResponse ? (
           <LoaderResponse />
         ) : resLenghtValid ? (
           <NoContentComponent />
-        ) : ( */}
-        <div>
-          <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 m-5">
-            <Card>
-              <h2 className="m-6 text-2xl font-bold">Resumo do Faturamento</h2>
-              <div className="m-6 d-flex grid flex-col gap-4">
-                <p className="text-base">
-                  Total Semanal: R${" "}
-                  <span className="text-base font-bold">
-                    {dadosRelatorio?.totalSemanal}
-                  </span>
-                </p>
-                <p className="text-base">
-                  Quantidade de Agendamentos:{" "}
-                  <span className="text-base font-bold">
-                    {dadosRelatorio?.qtdAgendas}
-                  </span>{" "}
-                  agendamentos
-                </p>
-                <p className="text-base">
-                  Serviços Mais Pedidos:{" "}
-                  <span className="text-base font-bold">
-                    {dadosRelatorio?.servicoMaisPedidos}
-                  </span>
-                </p>
-                <p className="text-base">
-                  Serviços Menos Pedidos:{" "}
-                  <span className="text-base font-bold">
-                    {dadosRelatorio?.servicoMenosPedidos}
-                  </span>
-                </p>
-              </div>
-            </Card>
-            <Card>
-              <h2 className="m-6 text-2xl font-bold">Resumo Semanal</h2>
-              <div className="m-6 d-flex grid flex-col gap-4">
-                <p className="text-base">Faturamento Máx:</p>
-                <span className="text-base font-bold">
-                  {dadosRelatorio?.faturamentoMaximo}
-                </span>
-                <p className="text-base">Faturamento Mín: </p>
-                <span className="text-base font-bold">
-                  {dadosRelatorio?.faturamentoMinimo}
-                </span>
+        ) : (
+          <div>
+            <div className="grid lg:grid-cols-2 grid-cols-1 gap-5 m-5">
+              <Card>
+                <h2 className="m-6 text-2xl font-bold">
+                  Resumo do Faturamento
+                </h2>
+                <div className="m-6 d-flex grid flex-col gap-4">
+                  <p className="text-base">
+                    Total: R${" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.totalSemanal}
+                    </span>
+                  </p>
+                  <p className="text-base">
+                    Quantidade de Agendamentos:{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.qtdAgendas}
+                    </span>{" "}
+                    agendamentos
+                  </p>
+                  <p className="text-base">
+                    Serviços Mais Pedidos:{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.servicoMaisPedidos}
+                    </span>
+                  </p>
+                  <p className="text-base">
+                    Serviços Menos Pedidos:{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.servicoMenosPedidos}
+                    </span>
+                  </p>
+                  <p className="text-base">
+                    Faturamento Máx:{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.faturamentoMaximo}
+                    </span>
+                  </p>
+
+                  <p className="text-base">
+                    Faturamento Mín:{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.faturamentoMinimo}
+                    </span>
+                  </p>
+                </div>
+              </Card>
+              <Card>
+                <h2 className="m-6 text-2xl font-bold">Top 5 Servicos</h2>
+                <div className="m-6 d-flex grid flex-col gap-4">
+                  <p className="text-base">
+                    1.{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.topServicoPrimeiro}
+                    </span>
+                  </p>
+                  <p className="text-base">
+                    2.{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.topServicoSegundo}
+                    </span>
+                  </p>
+                  <p className="text-base">
+                    3.{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.topServicoTerceiro}
+                    </span>
+                  </p>
+                  <p className="text-base">
+                    4.{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.topServicoQuarto}
+                    </span>
+                  </p>
+                  <p className="text-base">
+                    5.{" "}
+                    <span className="text-base font-bold">
+                      {dadosRelatorio?.topServicoQuinto}
+                    </span>
+                  </p>
+                </div>
+                <div className="m-6 d-flex grid flex-col gap-4"></div>
+              </Card>
+            </div>
+            <Card className="m-5">
+              <h2 className="m-6 text-2xl font-bold">Faturamento Semanal</h2>
+              <div className="m-6 h-full">
+                {dadosChart && dadosChart.length > 0 ? (
+                  <BarChart
+                    dataset={dataset}
+                    xAxis={[
+                      {
+                        data: [
+                          "Domingo",
+                          "Segunda",
+                          "Terça",
+                          "Quarta",
+                          "Quinta",
+                          "Sexta",
+                          "Sábado",
+                        ],
+                        scaleType: "band",
+                      },
+                    ]}
+                    yAxis={[{ min: valorMinimo, max: valorMaximo }]}
+                    series={[
+                      {
+                        label: "Quantidade",
+                        stack: "A",
+                        data: dadosChart.map((item) => item.total),
+                      },
+                      {
+                        label: "Rendimento",
+                        stack: "A",
+                        data: dadosChart.map((item) => item.rendimento),
+                      },
+                    ]}
+                    height={450}
+                  />
+                ) : (
+                  <p>Nenhum dado disponível para exibir o gráfico.</p>
+                )}
               </div>
             </Card>
           </div>
-          <Card className="m-5">
-            <h2 className="m-6 text-2xl font-bold">Faturamento Semanal</h2>
-            <div className="m-6">
-              {console.log(dadosChart)}
-              {dadosChart && dadosChart.length > 0 ? (
-                <BarChart
-                  xAxis={[
-                    {
-                      ...xAxis,
-                      tickLabelStyle: {
-                        angle: 45,
-                        dominantBaseline: "hanging",
-                        textAnchor: "start",
-                      },
-                      labelStyle: {
-                        transform: "translateY(15px)",
-                      },
-                    },
-                  ]}
-                  yAxis={[{ min: valorMinimo, max: valorMaximo }]}
-                  series={[
-                    {
-                      data: dadosChart.map((item) => item.total),
-                    },
-                  ]}
-                  height={350}
-                />
-              ) : (
-                <p>Nenhum dado disponível para exibir o gráfico.</p>
-              )}
-            </div>
-          </Card>
-        </div>
-        {/* )} */}
+        )}
       </section>
     </>
   );
--- src/pages/home-establishment/tabs/establishment-reports/reports-list/report-list.tsx
+++ src/pages/home-establishment/tabs/establishment-reports/reports-list/report-list.tsx
@@ -1,106 +1,116 @@
 import React, { useEffect, useState } from "react";
-import { Card } from "@mui/material";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import { Card, TextField, Button } from "@mui/material";
 import { AgendaRelatorio } from "../../../../../shared/entity/agenda";
 import { getAgendaRelatoriosContext } from "../../../../../shared/contexts/agendaContext";
-import AddchartIcon from "@mui/icons-material/Addchart";
-import { converterData } from "../../../../../shared/hooks/utils/validateInput";
-import { Link } from "react-router-dom";
-import LoaderResponse from "../../../../../components/loaderResponse";
-import NoContentComponent from "../../../../../components/noContent";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { SubmitHandler, useForm } from "react-hook-form";
+import dayjs, { Dayjs } from "dayjs";
+import ReportDashboard from "../reports-dashboard/report-dashboard";
+
+interface IFormInputs {
+  dataInicio: Dayjs | null;
+  dataFim: Dayjs | null;
+}
 
 export default function ReportList() {
-  const [loadResponse, setloadResponse] = useState(false);
-  const [resLenghtValid, setResLenghtValid] = useState(false);
-  const [ultimosRelatorios, setUltimosRelatorios] = useState<AgendaRelatorio[]>(
-    []
-  );
+  const [loadResponse, setLoadResponse] = useState(false);
+  const [resLengthValid, setResLengthValid] = useState(false);
+  const [ultimosRelatorios, setUltimosRelatorios] =
+    useState<AgendaRelatorio[]>();
 
-  useEffect(() => {
-    async function listarAgendaRelatorios() {
-      setloadResponse(false);
-      try {
-        const res = await getAgendaRelatoriosContext();
-        setUltimosRelatorios(res.data as AgendaRelatorio[]);
-        console.log(res);
-        if (res.data == undefined || res.data.length == 0) {
-          setResLenghtValid(true);
-        }
-        setloadResponse(true);
-      } catch (error) {
-        console.error("Erro ao buscar relatórios:", error);
-      }
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [formData, setFormData] = useState<{
+    dataInicio: string | null;
+    dataFim: string | null;
+  }>({
+    dataInicio: null,
+    dataFim: null,
+  });
+
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    defaultValues: {
+      dataInicio: null,
+      dataFim: null,
+    },
+  });
+
+  const handleFilterClick = () => {
+    setIsFormSubmitted(false);
+    setTimeout(() => {
+      setIsFormSubmitted(true);
+    }, 0);
+  };
+
+  const onSubmit: SubmitHandler<IFormInputs> = (data, event) => {
+    if (event) {
+      event.preventDefault();
     }
+    const formattedData = {
+      dataInicio: data.dataInicio
+        ? dayjs(data.dataInicio).format("YYYY-MM-DD")
+        : null,
+      dataFim: data.dataFim ? dayjs(data.dataFim).format("YYYY-MM-DD") : null,
+    };
+    setFormData(formattedData);
 
-    listarAgendaRelatorios();
-  }, []);
+    handleFilterClick();
+    if (IsFormSubmitted) {
+      setIsFormSubmitted(false);
+    }
+    setIsFormSubmitted(true);
+  };
+
   return (
     <>
       <Card className="m-5">
         <h2 className="m-6 text-2xl font-bold">Relatório do Estabelecimento</h2>
         <div className="m-6">
-          {!loadResponse ? (
-            <LoaderResponse />
-          ) : resLenghtValid ? (
-            <NoContentComponent />
-          ) : (
-            <TableContainer className="shadow-table" component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow className="font-bold">
-                    <TableCell align="center">Data Inicio</TableCell>
-                    <TableCell align="center">Data Final</TableCell>
-                    <TableCell align="center">Faturamento</TableCell>
-                    <TableCell align="center">Status</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {ultimosRelatorios
-                    ?.filter((relatorio) => relatorio != null)
-                    .map((relatorio) => (
-                      <TableRow
-                        key={relatorio?.dataInicio}
-                        sx={{
-                          "&:last-child td, &:last-child th": { border: 0 },
-                          marginBottom: 100,
-                        }}
-                      >
-                        <TableCell align="center" component="th" scope="row">
-                          {converterData(relatorio?.dataInicio.toString())}
-                        </TableCell>
-                        <TableCell align="center">
-                          {converterData(relatorio?.dataFinal.toString())}
-                        </TableCell>
-                        <TableCell align="center">
-                          R$ {relatorio?.precoTotal}
-                        </TableCell>
-                        <TableCell align="center">
-                          <Link
-                            to={`/establishment/reports/dashboard?dataInicio=${converterData(
-                              relatorio?.dataInicio.toString()
-                            )}&dataFim=${converterData(
-                              relatorio?.dataFinal.toString()
-                            )}`}
-                            underline="hover"
-                            className="cursor-pointer"
-                          >
-                            <AddchartIcon />
-                          </Link>
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          )}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex w-full row gap-4 flex-wrap justify-between"
+            >
+              <div className="gap-12 flex">
+                <DatePicker
+                  className="w-96"
+                  label="Data Início"
+                  renderInput={(params) => <TextField {...params} />}
+                  onChange={(newValue) => setValue("dataInicio", newValue)}
+                />
+                {errors.dataInicio && <p>{errors.dataInicio.message}</p>}
+                <DatePicker
+                  className="w-96"
+                  label="Data Final"
+                  renderInput={(params) => <TextField {...params} />}
+                  onChange={(newValue) => setValue("dataFim", newValue)}
+                />
+                {errors.dataFim && <p>{errors.dataFim.message}</p>}
+              </div>
+
+              <Button
+                className="float-start w-72 bg-primary-aqua h-14 "
+                variant="contained"
+                type="submit"
+              >
+                Filtrar
+              </Button>
+            </form>
+          </LocalizationProvider>
         </div>
       </Card>
+      {isFormSubmitted && (
+        <ReportDashboard
+          dataInicio={formData.dataInicio}
+          dataFim={formData.dataFim}
+        />
+      )}
     </>
   );
 }
