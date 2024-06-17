508ecc855a6b708b341d534edeabf329c61ef864
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-06-16 21:53:00 -0300

    build: feito o build

--- dist/assets/establishment-reports-0fdb4589.js
+++
@@ -1,1 +0,0 @@
-import{j as e,O as s}from"./index-6eff42d1.js";function r(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{r as default};
