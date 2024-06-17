95701d2612b1b8ab425fe6d73fc69b433c08a5d1
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2024-06-16 23:00:53 -0300

    build: env build deploy

---
+++ dist/assets/establishment-reports-a548a3db.js
@@ -0,0 +1,1 @@
+import{j as e,O as s}from"./index-2e3d6a2d.js";function r(){return e.jsx(e.Fragment,{children:e.jsx("section",{className:"w-full h-full",children:e.jsx(s,{})})})}export{r as default};
